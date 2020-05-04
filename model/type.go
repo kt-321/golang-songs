c22f46a819d896c18967dad9c41bf117ae407e0f
kt-321 <kt.shzk247@gmail.com>
2020-05-04 16:42:43 +0900

    構造体のタグの修正

--- model/type.go
+++ model/type.go
@@ -4,12 +4,11 @@ import "github.com/jinzhu/gorm"
 
 type User struct {
 	gorm.Model
-	Name  string `json:"name"`
-	Email string `json:"email,omitempty"`
-	Age   int    `json:"age,omitempty"`
-	//Age              int    `json:"age"`
+	Name             string `json:"name"`
+	Email            string `json:"email,omitempty"`
+	Age              int    `json:"age,omitempty"`
 	Gender           int    `json:"gender,omitempty"`
-	ImageUrl         string `json:"age,omitempty"`
+	ImageUrl         string `json:"imageUrl,omitempty"`
 	FavoriteMusicAge int    `json:"favoriteMusicAge,omitempty"`
 	FavoriteArtist   string `json:"favoriteArtist,omitempty"`
 	Comment          string `json:"comment,omitempty"`
