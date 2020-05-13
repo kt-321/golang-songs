9a70e569e878a750c09ab7ade03f6416df383c61
kt-321 <kt.shzk247@gmail.com>
2020-05-14 01:37:16 +0900

    不要な記述削除

--- controller/spotify.go
+++ controller/spotify.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"golang-songs/model"
@@ -114,20 +113,6 @@ func GetToken(w http.ResponseWriter, r *http.Request) {
 		errorInResponse(w, http.StatusInternalServerError, error)
 		return
 	}
-}
-
-func JSONSafeMarshal(v interface{}, safeEncoding bool) ([]byte, error) {
-	b, err := json.Marshal(v)
-	if err != nil {
-		return nil, err
-	}
-
-	if safeEncoding {
-		b = bytes.Replace(b, []byte("\\u003c"), []byte("<"), -1)
-		b = bytes.Replace(b, []byte("\\u003e"), []byte(">"), -1)
-		b = bytes.Replace(b, []byte("\\u0026"), []byte("&"), -1)
-	}
-	return b, err
 }
 
 func GetTracks(w http.ResponseWriter, r *http.Request) {
