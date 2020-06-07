6acf746d5763fd88101d92af91b46ff819bab788
kt-321 <kt.shzk247@gmail.com>
2020-06-07 17:45:29 +0900

    不要なコメントアウト削除

--- interfaces/auth_repository.go
+++ interfaces/auth_repository.go
@@ -12,9 +12,6 @@ type AuthRepository struct {
 
 func (ar *AuthRepository) SignUp(form model.Form) error {
 	if err := ar.DB.Create(&model.User{Email: form.Email, Password: form.Password}).Error; err != nil {
-		//var error model.Error
-		//error.Message = "アカウントの作成に失敗しました"
-		//errorInResponse(w, http.StatusUnauthorized, error)
 		return err
 	}
 	return nil
@@ -24,9 +21,6 @@ func (ar *AuthRepository) Login(form model.Form) (*model.User, error) {
 	var user model.User
 
 	if err := ar.DB.Where("email = ?", form.Email).Find(&user).Error; gorm.IsRecordNotFoundError(err) {
-		//var error model.Error
-		//error.Message = "該当するアカウントが見つかりません。"
-		//errorInResponse(w, http.StatusUnauthorized, error)
 		return nil, err
 	}
 	return &user, nil
