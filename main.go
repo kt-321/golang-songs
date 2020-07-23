c9753ce3cab7a800169179510f6edfa529afcae0
kt-321 <kt.shzk247@gmail.com>
2020-07-23 11:25:57 +0900

    Mysqlの接続情報修正

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"golang-songs/infrastructure"
 	"log"
+
 	//"os"
 
 	"github.com/jinzhu/gorm"
@@ -16,7 +17,7 @@ func main() {
 	//}
 
 	//一旦直打ち
-	db, err := gorm.Open("mysql", "mysqlConfig=kt321:route666@tcp(database-2.cvzte0rjvtt7.ap-northeast-1.rds.amazonaws.com:3306)/db_goyoursongs_rds?charset=utf8&parseTime=True")
+	db, err := gorm.Open("mysql", "kt321:route666@tcp(database-2.cvzte0rjvtt7.ap-northeast-1.rds.amazonaws.com:3306)/db_goyoursongs_rds?charset=utf8&parseTime=True")
 	if err != nil {
 		log.Println(err)
 	}
