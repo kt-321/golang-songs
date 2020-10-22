943ca3c48dbb38606413133a53f2283ee822d15e
kt-321 <kt.shzk247@gmail.com>
2020-10-22 21:28:15 +0900

    #137 サイドカーのRedisとの接続の記述追加

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	db.DB().SetMaxIdleConns(10)
 	defer db.Close()
 
-	//Redisのコネクションプールの設定
+	//リモートのRedisのコネクションプールの設定
 	pool := &redis.Pool{
 		MaxIdle:     3,
 		MaxActive:   6,
@@ -34,12 +34,39 @@ func main() {
 		},
 	}
 
+	//サイドカーのRedisのコネクションプール設定
+	pool2 := &redis.Pool{
+		MaxIdle:     3,
+		MaxActive:   6,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			rc, err := redis.Dial("tcp", os.Getenv("SIDECAR_REDIS_HOST"))
+			if err != nil {
+				return nil, err
+			}
+			return rc, nil
+		},
+	}
+
+	//リモートRedis
 	// コネクションの取得
 	redis := pool.Get()
 	if redis.Err() != nil {
 		log.Println(redis.Err())
+	} else {
+		log.Println("リモートのRedisに接続成功")
 	}
 	defer redis.Close()
+
+	//サイドカーRedis
+	// コネクションの取得
+	redis2 := pool2.Get()
+	if redis2.Err() != nil {
+		log.Println(redis2.Err())
+	} else {
+		log.Println("サイドカーのRedisに接続成功")
+	}
+	defer redis2.Close()
 
 	infrastructure.Dispatch(db, redis)
 }
