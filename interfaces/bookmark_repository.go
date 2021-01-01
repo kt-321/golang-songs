40fbf4278a307844580b9900c29fc659984b3b06
kt-321 <kt.shzk247@gmail.com>
2021-01-01 15:48:09 +0900

    #176 お気に入り登録・解除の処理において、メソッドFirstOrCreate使用/内部結合使用

--- interfaces/bookmark_repository.go
+++ interfaces/bookmark_repository.go
@@ -1,9 +1,8 @@
 package interfaces
 
 import (
-	"golang-songs/model"
-
 	"github.com/jinzhu/gorm"
+	"golang-songs/model"
 )
 
 type BookmarkRepository struct {
@@ -12,27 +11,26 @@ type BookmarkRepository struct {
 
 // 曲をお気に入り登録.
 func (br *BookmarkRepository) Bookmark(userEmail string, songID int) error {
+	// リクエストユーザーを取得.
 	var user model.User
 	if err := br.DB.Where("email = ?", userEmail).Find(&user).Error; gorm.IsRecordNotFoundError(err) {
 		return err
 	}
 
+	// お気に入り登録する対象の曲を取得.
 	var song model.Song
 	if err := br.DB.Where("id = ?", songID).Find(&song).Error; gorm.IsRecordNotFoundError(err) {
 		return err
 	}
 
-	if err := br.DB.Create(&model.Bookmark{
-		UserID: user.ID,
-		SongID: song.ID}).Error; err != nil {
-		return err
-	}
-
-	if err := br.DB.Preload("Bookmarkings").Find(&user).Error; err != nil {
-		return err
-	}
-
-	if err := br.DB.Model(&user).Association("Bookmarkings").Append(&song).Error; err != nil {
+	// deleted_atがnullであるbookmarksレコードがある時はレコード追加せず、該当レコードがない時はレコード追加する.
+	if err := br.DB.
+		Where("user_id = ?", user.ID).
+		Where("song_id = ?", song.ID).
+		FirstOrCreate(&model.Bookmark{
+			UserID: user.ID,
+			SongID: song.ID}).
+		Error; err != nil {
 		return err
 	}
 
@@ -41,22 +39,19 @@ func (br *BookmarkRepository) Bookmark(userEmail string, songID int) error {
 
 // 曲をお気に入り登録から解除.
 func (br *BookmarkRepository) RemoveBookmark(userEmail string, songID int) error {
-	var user model.User
-	if err := br.DB.Where("email = ?", userEmail).Find(&user).Error; gorm.IsRecordNotFoundError(err) {
+	var bookmark model.Bookmark
+
+	// bookmarksテーブルをusersテーブルやsongsテーブルと内部結合して、該当するレコードを取得する.
+	if err := br.DB.Table("bookmarks").
+		Where("bookmarks.deleted_at is null").
+		Joins("INNER JOIN users ON users.id = bookmarks.user_id AND users.email = ? AND users.deleted_at is null", userEmail).
+		Joins("INNER JOIN songs ON songs.id = bookmarks.song_id AND songs.id = ? AND songs.deleted_at is null", songID).
+		Scan(&bookmark).Error; gorm.IsRecordNotFoundError(err) {
 		return err
 	}
 
-	var song model.Song
-
-	if err := br.DB.Where("id = ?", songID).Find(&song).Error; gorm.IsRecordNotFoundError(err) {
-		return err
-	}
-
-	if err := br.DB.Preload("Bookmarkings").Find(&user).Error; err != nil {
-		return err
-	}
-
-	if err := br.DB.Model(&user).Association("Bookmarkings").Delete(&song).Error; err != nil {
+	// bookmarksレコードを論理削除.
+	if err := br.DB.Delete(&bookmark).Error; err != nil {
 		return err
 	}
 
